02e56b284dc430165f27ddf46170a91e11360726
barrelmaker <cooperleong0@gmail.com>
2023-07-29 21:53:30 -0400

    Cleanup and add variables

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,11 @@
 import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
-	url: 'http://127.0.0.1:4000/graphql',
+	url: import.meta.env.VITE_GRAFBASE_API_URL,
 	fetchParams({ session }) {
 		return {
 			headers: {
-				'x-api-key': '' // get this from .env in for prod. Can be an empty string in dev.
+				'x-api-key': import.meta.env.VITE_GRAFBASE_API_KEY
 			}
 		};
 	}
